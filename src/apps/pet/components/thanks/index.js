74f0b7fbe73d8ddf4262767c8bba1755b673ba33
CK Lam <ckhk0621@gmail.com>
2021-01-19 12:46:32 +0800

    [WIP] Thanks page mobile section

--- src/apps/pet/components/thanks/index.js
+++ src/apps/pet/components/thanks/index.js
@@ -1,71 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as themeActions from "store/actions/action-types/theme-actions";
-import { FlexboxGrid, ButtonToolbar, IconButton,Icon, fluid, gutter, Row, Col, Grid } from "rsuite";
+import { FlexboxGrid, ButtonToolbar, Button, IconButton,Icon, fluid, gutter, Row, Col, Grid } from "rsuite";
 import { connect } from "react-redux";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import axios from 'axios'
 import fileDownload from 'js-file-download'
+import wallpaper from "../../../../data/wallpaper.json"
 
 // install Swiper components
 SwiperCore.use([Scrollbar, A11y]);
 
-const Thanks = () => {
+const handleDownload = (url, filename) => {
+  axios.get(url, {
+    responseType: 'blob',
+  })
+  .then((res) => {
+    fileDownload(res.data, filename)
+  })
+}
 
-  const handleDownload = (url, filename) => {
-    axios.get(url, {
-      responseType: 'blob',
-    })
-    .then((res) => {
-      fileDownload(res.data, filename)
-    })
-  }
+const Thanks = () => {
+  const [Arctic, setArctic] = useState([]);
+  const [Forests, setForests] = useState([]);
+  const [Oceans, setOceans] = useState([]);
+  const [current, setCurrent] = useState([]);
+  const [download, setDownload] = useState("/wallpaper/Forests/GP0STO5DC_Web_size.jpg");
+
+  useEffect(() => {
+
+    setArctic(wallpaper.data.find(d=>d.issue==="Arctic"))
+    setForests(wallpaper.data.find(d=>d.issue==="Forests"))
+    setOceans(wallpaper.data.find(d=>d.issue==="Oceans"))
+
+  }, [wallpaper]);
+
+  // console.log('Arctic--', Arctic)
+  // console.log('Forests--', Forests)
+  // console.log('Oceans--', Oceans)
+
+  console.log('download--', download)
 
   return (
       <div className="show-grid full-height">
       <FlexboxGrid align="middle" justify="space-around" className="full-height">
-        <FlexboxGrid.Item colspan={9}>
+        <FlexboxGrid.Item componentClass={Col} xs={24} md={9}>
           <div className="thanks-content">
-            <h1>感謝您的聯署!</h1>
-              <p>
-              為維持公正獨立，持續向政府、企業提出訴求，綠色和平100%倚賴如您一般的支持者捐助，以直接行動帶來改變。您願意加入我們嗎？
-              清空原始森林及重要生態系統，換來畜牧場和大豆種植園；大火帶來的大量碳排放，更加劇全球氣候危機
-            </p>
-          </div>
-        </FlexboxGrid.Item>
-        <FlexboxGrid.Item colspan={15}>
-          <div className="thanks-download-image-wrap">
-            <img src="https://www.greenpeace.org/static/planet4-taiwan-stateless/2020/06/230499e4-gp036zk_medium_res-polar-bears-in-canada.jpg" className="img"/>
-            <ButtonToolbar>
-              <IconButton icon={<Icon icon="download" />} onClick={() => {handleDownload('http://localhost:3000/wallpaper/demo.jpg', 'wallpaper-01.jpg')}}>下載</IconButton>
-            </ButtonToolbar>
-          </div>
-        </FlexboxGrid.Item>
-        <FlexboxGrid.Item colspan={24} align="bottom">
-          <div className="thanks-slider-wrap">
-          <Swiper
-            spaceBetween={5}
-            slidesPerView={1}
-            navigation
-            // observer={true}
-            // observeParents={true}
-            // parallax={true}
-            // scrollbar={{ draggable: true }}
-          >
-            <SwiperSlide>
-              <Grid fluid>
-                <Row gutter={16}>
-                {Array.from({ length: 16 }).map(d=>
-                    <Col xs={3}><img src="https://www.greenpeace.org/static/planet4-taiwan-stateless/2020/06/230499e4-gp036zk_medium_res-polar-bears-in-canada.jpg" className="img wallpaper-thumbnail"/></Col>
-                )}
+            <h3>感謝您的下載！</h3>
+              <p>您願意進一步行動，捐助支持綠色和平更多環境項目嗎？</p>
+              <p>守護海洋，保衛森林，我們需要您的支持為環境堅持努力。綠色和平不接受政府、企業捐款，請立刻加入我們的<b>1%會員計畫，以您的1%收入，支持我們的100%財政獨立。</b></p>
+              {/* <Grid fluid>
+                <Row gutter={12}>
+                  <Col xs={12}><Button color="green" appearance='default' block><b>捐助支持</b></Button></Col>
+                  <Col xs={12}><Button color="violet" appearance='default' block><b>分享</b></Button></Col>
                 </Row>
-              </Grid>
-            </SwiperSlide>
+              </Grid> */}
+          </div>
+        </FlexboxGrid.Item>
+        <FlexboxGrid.Item componentClass={Col} xs={24} md={15}>
+          <div className="thanks-download-image-wrap">
+            <img src={download} className="img"/>
+            <div className="thanks-mobile-background-image" style={{backgroundImage: `url(${download})`}}></div>
+            <div className="mobile-download">
+              <ButtonToolbar>
+                <IconButton icon={<Icon icon="download" />} onClick={() => {handleDownload(download, 'wallpaper-01.jpg')}}>下載</IconButton>
+              </ButtonToolbar>
+            </div>
+          </div>
+        </FlexboxGrid.Item>
+        <FlexboxGrid.Item componentClass={Col} colspan={24} align="bottom">
+          <div className="thanks-slider-wrap">
+          <Swiper spaceBetween={5} slidesPerView={1}>
             <SwiperSlide>
               <Grid fluid>
                 <Row gutter={16}>
-                {Array.from({ length: 16 }).map(d=>
-                    <Col xs={3}><img src="https://www.greenpeace.org/static/planet4-taiwan-stateless/2020/06/230499e4-gp036zk_medium_res-polar-bears-in-canada.jpg" className="img wallpaper-thumbnail"/></Col>
+                {Forests.content?.wallpaperList.map((d, i) =>
+                  <Col key={i} xs={12} md={4} className="wallpaper-thumbnail-col" onClick={()=>setDownload(d)}>
+                    <div className="img wallpaper-thumbnail wallpaper-thumbnail-mobile" style={{backgroundImage: `url(${d})`}}></div>
+                  </Col>
                 )}
                 </Row>
               </Grid>
@@ -74,6 +86,12 @@ const Thanks = () => {
           </div>
         </FlexboxGrid.Item>
       </FlexboxGrid>
+      {/* <a href="#" className="start-btn cta-custom-display">
+        <span>
+          {" "}
+          立即聯署
+        </span>
+      </a> */}
       </div>
   );
 };
