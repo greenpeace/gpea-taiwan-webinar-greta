25a725763bcd8961c72163c9879bdf1409ddb84b
KFC <hankfchan@gmail.com>
2021-01-20 11:12:51 +0800

    conflict solved

--- src/apps/pet/components/main/index.js
+++ src/apps/pet/components/main/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-=======
-import React, {useEffect} from 'react';
->>>>>>> 4a62dba913dbf984924db4da1dfefcf9c2e3b219
+import React, { useEffect } from "react";
 import * as themeActions from "store/actions/action-types/theme-actions";
 import { connect } from "react-redux";
 import Header from "apps/pet/components/header";
@@ -11,11 +7,7 @@ import Content from "./contentScreen";
 import Panel from "../../../../components/panel";
 import RegistrationForm from "../../../../components/form/registrationForm";
 
-<<<<<<< HEAD
-const Index = ({ submitted }) => {
-  return (
-=======
-const Index = ({initFormState, fakeSubmit, submitted}) => {
+const Index = ({ initFormState, fakeSubmit, submitted }) => {
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -26,31 +18,32 @@ const Index = ({initFormState, fakeSubmit, submitted}) => {
     }
   }, []);
 
-  return (<>
->>>>>>> 4a62dba913dbf984924db4da1dfefcf9c2e3b219
-    <div id="main" className={submitted ? "custom-main" : ""}>
-      <Header />
-      <div id="wrapper">
-        <div className="content full-height" data-pagetitle="Home slider">
-          <div className="fl-wrap full-height hero-conatiner">
-            <article className="prose lg:prose-lg">
-              {submitted ? <Thanks /> : <Content />}
-            </article>
+  return (
+    <>
+      <div id="main" className={submitted ? "custom-main" : ""}>
+        <Header />
+        <div id="wrapper">
+          <div className="content full-height" data-pagetitle="Home slider">
+            <div className="fl-wrap full-height hero-conatiner">
+              <article className="prose lg:prose-lg">
+                {submitted ? <Thanks /> : <Content />}
+              </article>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="custom-form-wrap">
-      {submitted ? (
-        <div className="custom-gp-form custom-gp-form-wrap">
-          <div className="custom-bg"></div>
-        </div>
-      ) : (
-        <RegistrationForm />
-      )}
-    </div>
-    <Panel />
-  </>);
+      <div className="custom-form-wrap">
+        {submitted ? (
+          <div className="custom-gp-form custom-gp-form-wrap">
+            <div className="custom-bg"></div>
+          </div>
+        ) : (
+          <RegistrationForm />
+        )}
+      </div>
+      <Panel />
+    </>
+  );
 };
 
 const mapStateToProps = ({ theme }) => {
