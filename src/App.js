0201f02cf071ed7d050786c90c91ec578c76ce4f
KFC <hankfchan@gmail.com>
2021-01-20 23:47:01 +0800

    sort the styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import * as themeActions from "store/actions/action-types/theme-actions";
-import "App.less";
+
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 import "swiper/components/scrollbar/scrollbar.scss";
 import "rsuite/lib/styles/index.less";
 import "react-sliding-pane/dist/react-sliding-pane.css";
-import "form.less";
+import "App.less";
+
 import Main from "apps/pet/components/main";
 import Spinner from "components/spinner";
-import ScrollHandler from "components/spinner/scrollHandler"
+import ScrollHandler from "components/spinner/scrollHandler";
 
 let App = ({ loading }) => {
   const [blockScroll, allowScroll] = ScrollHandler();
   useEffect(() => {
-    loading ? blockScroll() : allowScroll()
-  }, [loading])
-  
+    loading ? blockScroll() : allowScroll();
+  }, [loading]);
+
   return (
     <div className="App">
       {loading && <Spinner />}
