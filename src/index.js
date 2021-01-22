e530191d0ced8690a9a337ba984504db3d7b403d
KFC <hankfchan@gmail.com>
2021-01-22 14:55:10 +0800

    preview

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
-import './index.css';
-import './fontawesome';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import "./index.css";
+import "./fontawesome";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 ReactDOM.render(
   // <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <title>Greenpeace 綠色和平 | 香港</title>
-      </Helmet>
-      <Provider store={configureStore}>
-        <App />
-      </Provider>
-    </HelmetProvider>,
+  <HelmetProvider>
+    <Helmet></Helmet>
+    <Provider store={configureStore}>
+      <App />
+    </Provider>
+  </HelmetProvider>,
   // </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
