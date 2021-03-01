35206986858f32ba01cc691929e7caf07e01e574
KFC <hankfchan@gmail.com>
2021-03-01 12:11:54 +0800

    update react-snap & build scripts

--- mc-deploy.js
+++ mc-deploy.js
@@ -36,9 +36,9 @@ Install the dependencies first
 // definitions
 const buildFolder = path.join(__dirname, "build");
 const EndpointURL = "https://cloud.greenhk.greenpeace.org/petition-pp";
-let CampaignId = "7012u000000P1itAAC";
+let CampaignId = "7012u000000P2LIAA0";
 const DonationPageUrl = "https://www.greenpeace.org/eastasia/"; // not used now
-const interests = ["Health"]; // Arctic, Climate, Forest, Health, Oceans, Plastics
+const interests = ["Oceans"]; // Arctic, Climate, Forest, Health, Oceans, Plastics
 const ftpConfigName = "ftp_hk"; // refer to ~/.npm-en-uploader-secret
 const ftpRemoteDir = "/2021/fukushima";
 
