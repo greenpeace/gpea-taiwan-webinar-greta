8d6512cf24b894c0b898434db0386397faf81921
CK Lam <ckhk0621@gmail.com>
2021-03-09 01:08:34 +0800

    Hot fix IG ui issue

--- src/App.js
+++ src/App.js
@@ -15,15 +15,21 @@ import ScrollHandler from "components/spinner/scrollHandler";
 
 const Main = React.lazy(() => import(`apps/${process.env.REACT_APP_PROJECT}/components/main`));
 
+const isIGApp = () => {
+  var ua = navigator.userAgent || navigator.vendor || window.opera;
+  return (ua.indexOf("Instagram") > -1);
+}
+
 let App = ({ loading, submitted }) => {
   const [blockScroll, allowScroll] = ScrollHandler();
   const additionalClass = submitted ? `submitted-content` : ''
+  const isIG = isIGApp() ? 'isIG' : ''
   useEffect(() => {
     loading ? blockScroll() : allowScroll();
   }, [loading, blockScroll, allowScroll]);
 
   return (
-    <div className={`App ${additionalClass}`}>
+    <div className={`App ${additionalClass} ${isIG}`}>
       {loading && <Spinner />}
       <Suspense fallback={<Spinner />}>
         <Main />
