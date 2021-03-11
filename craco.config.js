b76033f9612ce68a908b5876a297ef8f9182938f
KFC <chris.chan@greenpeace.org>
2021-03-11 21:23:01 +0800

    craco plugins

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,31 @@
 const CracoLessPlugin = require("craco-less");
+const CompressionPlugin = require("compression-webpack-plugin"); //gzip
+const BrotliPlugin = require("brotli-webpack-plugin"); //brotli
+const imageOptimizer = require("craco-image-optimizer-plugin");
 
 module.exports = {
   plugins: [
+    {
+      plugin: CompressionPlugin,
+      options: {
+        //gzip plugin
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.(js|css|html|svg)$/,
+        threshold: 8192,
+        minRatio: 0.8,
+      },
+    },
+    {
+      plugin: BrotliPlugin,
+      options: {
+        //brotli plugin
+        asset: "[path].br[query]",
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      },
+    },
     {
       plugin: CracoLessPlugin,
       options: {
@@ -11,6 +35,31 @@ module.exports = {
           },
         },
         sourceMap: true,
+      },
+    },
+    {
+      plugin: imageOptimizer,
+      // image-webpack-plugin options
+      options: {
+        mozjpeg: {
+          progressive: true,
+          quality: 65,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.9],
+          speed: 4,
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75,
+        },
       },
     },
   ],
