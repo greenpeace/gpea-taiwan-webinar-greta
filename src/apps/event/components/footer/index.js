428f01ed80b26eb89d5e5e087c9fda4341983bcf
CK Lam <ckhk0621@gmail.com>
2021-04-11 14:04:17 +0800

    Clone for event

---
+++ src/apps/event/components/footer/index.js
@@ -0,0 +1,100 @@
+import {
+  Box,
+  Image,
+  chakra,
+  Container,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { ReactNode } from 'react';
+
+const Logo = (props) => {
+  return (
+    <Box>
+            <Image
+              maxW="150px"
+              objectFit="contain"
+              src="https://api.greenpeace.org.hk/general/logo/GP-logo-2019-TC-green-%5bweb%5d-01.png"
+            /></Box>
+  );
+};
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function SmallCentered() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        spacing={4}
+        justify={'center'}
+        align={'center'}>
+        <Logo />
+        <Stack direction={'row'} spacing={6}>
+          <Link href={'#'}>首頁</Link>
+          <Link href={'#'}>私隱政策與個人資料收集聲明</Link>
+          <Link href={'#'}>捐助支持</Link>
+        </Stack>
+      </Container>
+      <Box
+        borderTopWidth={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        <Container
+          as={Stack}
+          maxW={'6xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ base: 'center', md: 'space-between' }}
+          align={{ base: 'center', md: 'center' }}>
+          <Text>© GREENPEACE 2021</Text>
+          <Stack direction={'row'} spacing={6}>
+            <SocialButton label={'Twitter'} href={'#'}>
+              <FaTwitter />
+            </SocialButton>
+            <SocialButton label={'YouTube'} href={'#'}>
+              <FaYoutube />
+            </SocialButton>
+            <SocialButton label={'Instagram'} href={'#'}>
+              <FaInstagram />
+            </SocialButton>
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/apps/event/theme.js
@@ -0,0 +1,46 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#ddffa3",
+      300: "#a2e649",
+      500: "#66cc00", // Default GP
+      700: "#378000",
+      900: "#133300",
+    },
+    gray: {
+      100: "#f5f5f5",
+      300: "#e0e0e0",
+      500: "#9e9e9e",
+      700: "#616161",
+      900: "#212121",
+    },
+    orange: "#ff8100",
+    red: "#ff3333",
+    lightblue: "#f4f9fa",
+    blue: "#108ee9",
+    darkblue: "#292f47",
+    campaign: {
+      arctic: "#62cbd7",
+      health: "#f96d8c",
+      climate: "#ffbe00",
+      plastics: "#afaa91",
+      forests: "#00b474",
+      oceans: "#008fe2",
+    },
+  },
+  bg: {
+    campaign: {
+      arctic: "#62cbd7",
+      health: "#f96d8c",
+      climate: "#ffbe00",
+      plastics: "#afaa91",
+      forests: "#00b474",
+      oceans: "#008fe2",
+    },
+  }
+});
+
+export default theme;
